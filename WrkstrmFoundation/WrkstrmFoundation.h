a5bfea49be0557d31bc468f53c8b910d50a9731f
Cristian Monterroza <cmonterroza@wrkstrm.me>
2018-08-28 11:37:59 -0700

    Initial Commit

---
+++ WrkstrmFoundation/WrkstrmFoundation.h
@@ -0,0 +1,19 @@
+//
+//  WrkstrmFoundation.h
+//  WrkstrmFoundation
+//
+//  Created by Cristian Monterroza on 8/28/18.
+//  Copyright © 2018 Cristian Monterroza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WrkstrmFoundation.
+FOUNDATION_EXPORT double WrkstrmFoundationVersionNumber;
+
+//! Project version string for WrkstrmFoundation.
+FOUNDATION_EXPORT const unsigned char WrkstrmFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WrkstrmFoundation/PublicHeader.h>
+
+
