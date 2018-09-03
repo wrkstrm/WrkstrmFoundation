3d8bd43f750d3852589ee5358df22e362841437b
Cristian Monterroza <cmonterroza@wrkstrm.me>
2018-09-03 00:18:06 -0700

    Make URL convenience method easier. (+3 squashed commits) Squashed commits: [c04fef4] Add convenience init for setting URL directly. [f59f733] Remove warnings on not using result of saving. [f81ca4d] Allow for nullable keys to use the default keys. (+1 squashed commit) Squashed commits: [e01bc70] Improve Archiver ergonomics.

--- WrkstrmFoundation/Foundation/Persistence/CodableArchiver.swift
+++ WrkstrmFoundation/Foundation/Persistence/CodableArchiver.swift
@@ -11,6 +11,7 @@ import Foundation
 public struct CodableArchiver<T: Codable> {
 
     public let encoder: JSONEncoder
+
     public let decoder: JSONDecoder
 
     public let fileManager: FileManager = FileManager.default
@@ -20,15 +21,24 @@ public struct CodableArchiver<T: Codable> {
     public let key: AnyHashable
 
     public init(key: AnyHashable,
-                encoder: JSONEncoder,
-                decoder: JSONDecoder,
                 directory: FileManager.SearchPathDirectory,
-                searchPathDomainMask: FileManager.SearchPathDomainMask) {
+                encoder: JSONEncoder = .default,
+                decoder: JSONDecoder = .default,
+                searchPathDomainMask: FileManager.SearchPathDomainMask = [.allDomainsMask]) {
         self.encoder = encoder
         self.decoder = decoder
         let archiveDirectory = fileManager.urls(for: directory, in: searchPathDomainMask).first!
         self.archiveDirectory = archiveDirectory.appendingPathComponent(String(key.description))
         self.key = key
+    }
+
+    public init(directory: URL,
+                encoder: JSONEncoder = .default,
+                decoder: JSONDecoder = .default) {
+        self.encoder = encoder
+        self.decoder = decoder
+        self.archiveDirectory = directory.deletingLastPathComponent()
+        self.key = directory.lastPathComponent
     }
 }
 
@@ -44,8 +54,8 @@ extension CodableArchiver {
 
 extension CodableArchiver {
 
-    public func get(_ key: AnyHashable) -> T? {
-        guard let data = NSKeyedUnarchiver.unarchiveObject(withFile: filePathForKey(key)) as? Data else {
+    public func get(_ key: AnyHashable? = nil) -> T? {
+        guard let data = NSKeyedUnarchiver.unarchiveObject(withFile: filePathForKey(key ?? self.key)) as? Data else {
             return nil
         }
 
@@ -56,25 +66,27 @@ extension CodableArchiver {
         return decoded
     }
 
-    public func set(_ value: T, forKey key: AnyHashable) -> Bool {
+    @discardableResult
+    public func set(_ value: T, forKey key: AnyHashable? = nil) -> Bool {
         let data = try? encoder.encode(value)
         try? fileManager.createDirectory(at: archiveDirectory,
                                          withIntermediateDirectories: true,
                                          attributes: nil)
         if let data = data {
-            return NSKeyedArchiver.archiveRootObject(data, toFile: filePathForKey(key))
+            return NSKeyedArchiver.archiveRootObject(data, toFile: filePathForKey(key ?? self.key))
         } else {
             return false
         }
     }
 
-    public func set(_ value: [T], forKey key: AnyHashable) -> Bool {
+    @discardableResult
+    public func set(_ value: [T], forKey key: AnyHashable? = nil) -> Bool {
         let encodedValues = try? value.map { try encoder.encode($0) }
         try? fileManager.createDirectory(at: archiveDirectory,
                                          withIntermediateDirectories: true,
                                          attributes: nil)
         if let encodedValues = encodedValues {
-            return NSKeyedArchiver.archiveRootObject(encodedValues, toFile: filePathForKey(key))
+            return NSKeyedArchiver.archiveRootObject(encodedValues, toFile: filePathForKey(key ?? self.key))
         } else {
             return false
         }
