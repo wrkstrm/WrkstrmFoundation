214cba0127805577ba28a1097e20dde040c2207b
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-27 11:01:12 -0700

    Lint steps! (+2 squashed commits) Squashed commits: [4f256e0] Building. [4ae4b35] BASIC bash shell (+2 squashed commits) Squashed commits: [da33957] Update wrkstrm-shell-build.yml [69201ce] Remove deps from localize. Add them to Shell. (+1 squashed commit) Squashed commits: [bec8c50] Add project details for WrkstrmShell

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "WrkstrmFoundation",
   platforms: [
-    .iOS(.v15),
+    .iOS(.v14),
     .macOS(.v12),
     .tvOS(.v12),
     .watchOS(.v5),
