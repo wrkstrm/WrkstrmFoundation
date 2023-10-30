79592a9f09b0ae12f6943ed05b0f51767e15420a
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-30 12:18:11 -0700

    Update localize yml.

--- Sources/WrkstrmFoundationRT/NSObject+BitMaskProperty.swift
+++ Sources/WrkstrmFoundationRT/NSObject+BitMaskProperty.swift
@@ -5,10 +5,10 @@ import WrkstrmMain
 extension NSObject {
   /// Sets a bit mask to a property on an object.
   public func property(_ property: String, set: [UInt], clear: [UInt]) {
-    guard let value = self.value(forKey: property) as? UInt else {
+    guard let value = value(forKey: property) as? UInt else {
       Log.verbose("property (\(property)) cannot be interpreted as an UInt.")
       return
     }
-    self.setValue((value & ~UInt.bitSet(clear)) | UInt.bitSet(set), forKey: property)
+    setValue((value & ~UInt.bitSet(clear)) | UInt.bitSet(set), forKey: property)
   }
 }
