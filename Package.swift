8dfaf76e2ff00d838ac39cc180cd59d9be2eaedb
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-31 00:55:26 -0700

    Split WrkstrmFoundation.

--- Package.swift
+++ Package.swift
@@ -11,16 +11,12 @@ let package = Package(
   ],
   products: [
     .library(name: "WrkstrmFoundation", targets: ["WrkstrmFoundation"]),
-    .library(name: "WrkstrmFoundationRT", targets: ["WrkstrmFoundationRT"]),
-    .library(name: "WrkstrmFoundationObjC", type: .dynamic, targets: ["WrkstrmFoundationObjC"]),
   ],
   dependencies: [
     .package(name: "WrkstrmLog", path: "../WrkstrmLog"),
     .package(name: "WrkstrmMain", path: "../WrkstrmMain"),
   ],
   targets: [
-    .target(name: "WrkstrmFoundationObjC", dependencies: []),
     .target(name: "WrkstrmFoundation", dependencies: ["WrkstrmLog", "WrkstrmMain"]),
-    .target(name: "WrkstrmFoundationRT", dependencies: ["WrkstrmLog", "WrkstrmMain"]),
     .testTarget(name: "WrkstrmFoundationTests", dependencies: ["WrkstrmFoundation"]),
   ])
