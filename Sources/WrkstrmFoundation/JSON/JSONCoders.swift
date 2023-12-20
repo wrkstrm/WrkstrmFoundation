d1d39c97d4413945c61934097b32d15f5e521926
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-12-20 14:40:44 -0800

    Document JSON.

--- Sources/WrkstrmFoundation/JSON/JSONCoders.swift
+++ Sources/WrkstrmFoundation/JSON/JSONCoders.swift
@@ -1,11 +1,24 @@
 #if os(Linux)
-// Needed because DispatchQueue isn't Sendable on Linux
+// Necessary import for Linux due to DispatchQueue not being Sendable.
 @preconcurrency import Foundation
 #else
 import Foundation
 #endif
 
+/// An extension to `JSONDecoder` to provide a customized decoder for handling specific date formats.
 extension JSONDecoder {
+
+  /// A static instance of `JSONDecoder` with a custom date decoding strategy.
+  ///
+  /// This decoder uses a custom strategy for decoding dates from JSON. The strategy
+  /// understands multiple date formats, including ISO 8601 with and without milliseconds,
+  /// and a simple date-only format.
+  ///
+  /// Example:
+  /// ```
+  /// let jsonData = "{\"date\":\"2023-01-01T00:00:00Z\"}".data(using: .utf8)!
+  /// let decodedObject = try JSONDecoder.default.decode(MyDecodableType.self, from: jsonData)
+  /// ```
   public static let `default` = { () -> JSONDecoder in
     let decoder: JSONDecoder = .init()
     decoder.dateDecodingStrategy = .custom(Decoding.customDateDecoder)
@@ -13,7 +26,19 @@ extension JSONDecoder {
   }()
 }
 
+/// An extension to `JSONEncoder` to provide a customized encoder for handling specific date formats.
 extension JSONEncoder {
+
+  /// A static instance of `JSONEncoder` with a custom date encoding strategy.
+  ///
+  /// This encoder uses a custom strategy for encoding dates to JSON. The strategy
+  /// converts dates to an ISO 8601 string format.
+  ///
+  /// Example:
+  /// ```
+  /// let myObject = MyEncodableType(date: Date())
+  /// let jsonData = try JSONEncoder.default.encode(myObject)
+  /// ```
   public static let `default` = { () -> JSONEncoder in
     let encoder: JSONEncoder = .init()
     encoder.dateEncodingStrategy = .custom(Encoding.customDateEncoder)
@@ -21,7 +46,16 @@ extension JSONEncoder {
   }()
 }
 
+// MARK: - Private Helper Enums
+
+/// A private enum containing a custom date encoding method.
 private enum Encoding {
+  /// Encodes a `Date` object to a string using a specified date format.
+  ///
+  /// - Parameters:
+  ///   - date: The `Date` object to encode.
+  ///   - encoder: The `Encoder` to use for encoding the date.
+  /// - Throws: An encoding error if the date cannot be encoded.
   static func customDateEncoder(date: Date, encoder: Encoder) throws {
     let stringDate = DateFormatter.iso8601.string(from: date)
     var container = encoder.singleValueContainer()
@@ -29,9 +63,16 @@ private enum Encoding {
   }
 }
 
+/// A private enum containing a custom date decoding method.
 private enum Decoding {
+  /// Decodes a date string to a `Date` object using various date formats.
+  ///
+  /// - Parameter decoder: The `Decoder` to use for decoding the date string.
+  /// - Returns: The decoded `Date` object.
+  /// - Throws: A decoding error if the date string cannot be decoded to a `Date`.
   static func customDateDecoder(_ decoder: Decoder) throws -> Date {
     let dateString: String = try decoder.singleValueContainer().decode(String.self)
+    // Attempt to decode the date using various formats.
     if let date = DateFormatter.iso8601.date(from: dateString) {
       return date
     }
@@ -41,6 +82,7 @@ private enum Decoding {
     if dateString.count == 8, let date = DateFormatter.dateOnlyEncoder.date(from: dateString) {
       return date
     }
+    // Throw an error if none of the formats match.
     let error =
       DecodingError.Context(
         codingPath: decoder.codingPath,
--- Sources/WrkstrmFoundation/JSON/JSONEnums.swift
+++ Sources/WrkstrmFoundation/JSON/JSONEnums.swift
@@ -1,40 +1,70 @@
 import WrkstrmMain
 
 #if os(Linux)
-// Needed because DispatchQueue isn't Sendable on Linux
+// Necessary import for Linux due to DispatchQueue not being Sendable.
 @preconcurrency import Foundation
 #else
 import Foundation
 #endif
 
+/// Extensions to the `WrkstrmMain.JSON` namespace to provide additional JSON value representations.
 extension WrkstrmMain.JSON {
+
+  /// An enum representing arrays with equatable functionality.
+  ///
+  /// This enum provides a way to handle arrays in a type-safe manner with the capability
+  /// of equality comparison. It includes various cases for different types of arrays
+  /// that can be encountered in JSON data.
   public enum AnyEquatableArrayEnum: Equatable {
+    /// Represents a generic array of `Any` type.
     case any([Any])
 
+    /// Represents an array of dictionaries, with each dictionary conforming to `JSON.AnyDictionary`.
     case dictionary([JSON.AnyDictionary])
 
+    /// A static method to compare two `AnyEquatableArrayEnum` values.
+    /// Currently, this implementation always returns `true`.
     public static func == (_: Self, _: Self) -> Bool { true }
   }
 
+  /// An enum representing dictionaries with equatable functionality.
+  ///
+  /// This enum is designed to handle dictionaries in JSON with equatable capabilities,
+  /// allowing for comparison operations.
   public enum AnyEquatableDictionaryEnum: Equatable {
+    /// Represents a dictionary of type `JSON.AnyDictionary`.
     case any(JSON.AnyDictionary)
 
+    /// A static method to compare two `AnyEquatableDictionaryEnum` values.
+    /// Currently, this implementation always returns `true`.
     public static func == (_: Self, _: Self) -> Bool { true }
   }
 
+  /// An enum representing various types of JSON values, each associated with a key.
+  ///
+  /// This enum is useful for representing and handling different kinds of JSON values
+  /// in a type-safe manner. Each case is associated with a string key and a value of
+  /// a specific type.
   public enum Value: Equatable {
+    /// Represents an integer value associated with a string key.
     case integer(String, Int)
 
+    /// Represents a double value associated with a string key.
     case double(String, Double)
 
+    /// Represents a string value associated with a string key.
     case string(String, String)
 
+    /// Represents a date value associated with a string key.
     case date(String, Date)
 
+    /// Represents an array value associated with a string key.
     case array(String, JSON.AnyEquatableArrayEnum)
 
+    /// Represents a dictionary value associated with a string key.
     case dictionary(String, JSON.AnyEquatableDictionaryEnum)
 
+    /// Represents a generic value associated with a string key.
     case any(String, String)
   }
 }
