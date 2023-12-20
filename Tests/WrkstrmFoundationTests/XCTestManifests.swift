dcbb334572c68a8fa697018c1eded03bda00e73f
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-12-20 15:04:12 -0800

    Remove unneeded tests.

--- Tests/WrkstrmFoundationTests/XCTestManifests.swift
+++ Tests/WrkstrmFoundationTests/XCTestManifests.swift
@@ -4,8 +4,6 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
   [
     testCase(CalendarTests.allTests),
-    testCase(CollectionTests.allTests),
-    testCase(StringTests.allTests),
   ]
 }
 #endif
