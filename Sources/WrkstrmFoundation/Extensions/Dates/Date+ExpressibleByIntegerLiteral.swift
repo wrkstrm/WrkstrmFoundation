2eb671f63c04c956350a4a7a3d86b2b1fbed5fab
Cristian A Monterroza <cmonterroza@laussatstudio.com>
2024-06-24 19:28:24 -0700

    Update Date+ExpressibleByIntegerLiteral.swift

--- Sources/WrkstrmFoundation/Extensions/Dates/Date+ExpressibleByIntegerLiteral.swift
+++ Sources/WrkstrmFoundation/Extensions/Dates/Date+ExpressibleByIntegerLiteral.swift
@@ -6,15 +6,16 @@ import Foundation
 #endif
 
 /// Extension of `Date` to conform to `ExpressibleByIntegerLiteral`.
-/// This allows `Date` instances to be initialized using an integer literal, representing the number of seconds since January 1, 1970.
+/// This allows `Date` instances to be initialized using an integer literal, representing the number
+/// of seconds since January 1, 1970.
 extension Date: ExpressibleByIntegerLiteral {
-    /// The type of integer literal used to initialize `Date`.
-    public typealias IntegerLiteralType = Int
+  /// The type of integer literal used to initialize `Date`.
+  public typealias IntegerLiteralType = Int
 
-    /// Initializes a new instance of `Date` using an integer literal.
-    /// The integer value represents the number of seconds since January 1, 1970 (Unix epoch time).
-    /// - Parameter value: An `Int` representing the number of seconds since the Unix epoch.
-    public init(integerLiteral value: Int) {
-        self.init(timeIntervalSince1970: Double(value))
-    }
+  /// Initializes a new instance of `Date` using an integer literal.
+  /// The integer value represents the number of seconds since January 1, 1970 (Unix epoch time).
+  /// - Parameter value: An `Int` representing the number of seconds since the Unix epoch.
+  public init(integerLiteral value: Int) {
+    self.init(timeIntervalSince1970: Double(value))
+  }
 }
