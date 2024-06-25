88dce8323f3347d392ab76b018767757b7764c80
Cristian A Monterroza <cmonterroza@laussatstudio.com>
2024-06-24 19:24:03 -0700

    Update Log+Foundation.swift

--- Sources/WrkstrmFoundation/Extensions/Log+Foundation.swift
+++ Sources/WrkstrmFoundation/Extensions/Log+Foundation.swift
@@ -3,8 +3,8 @@ import WrkstrmLog
 extension Log {
   /// A static instance of `Log` specifically configured for foundation-related messages.
   ///
-  /// This logger is tailored for logging messages in the "foundation" category of the "wrkstrm" system.
-  /// It sets a maximum function length to ensure consistency and readability in the logs.
+  /// This logger is tailored for logging messages in the "foundation" category of the "wrkstrm"
+  /// system. It sets a maximum function length to ensure consistency and readability in the logs.
   ///
   /// Usage:
   /// ```swift
