495e6233b9944edb4f0f15f84dc7776f5db18d7b
Cristian A Monterroza <cmonterroza@laussatstudio.com>
2024-06-24 19:22:44 -0700

    Update String+Expanding.swift

--- Sources/WrkstrmFoundation/Extensions/String/String+Expanding.swift
+++ Sources/WrkstrmFoundation/Extensions/String/String+Expanding.swift
@@ -2,7 +2,6 @@ import Foundation
 
 /// An extension to the `String` class providing a method to expand the tilde in file paths.
 extension String {
-
   /// Expands the tilde (`~`) in the string to the user's home directory path.
   ///
   /// This method is useful for converting path strings that use the tilde as a shorthand
@@ -15,7 +14,8 @@ extension String {
   /// // Prints the full path, e.g., "/Users/username/Documents"
   /// ```
   ///
-  /// - Returns: A new string where the tilde is replaced by the full path of the user's home directory.
+  /// - Returns: A new string where the tilde is replaced by the full path of the user's home
+  /// directory.
   public func homeExpandedString() -> String {
     (self as NSString).expandingTildeInPath
   }
