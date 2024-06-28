11d00376be05d34dcadae76e2d3f4b199dbe8904
Cristian A Monterroza <1240948+rismay@users.noreply.github.com>
2024-06-28 00:28:07 -0700

    Update JSONResource.swift

--- Sources/WrkstrmFoundation/JSON/JSONResource.swift
+++ Sources/WrkstrmFoundation/JSON/JSONResource.swift
@@ -1,16 +1,19 @@
 import Foundation
+import WrkstrmMain
 
-enum JSONResource {
-  static func load(fileName: String) -> Data? {
-    let currentFileURL = URL(fileURLWithPath: #file)
-    let currentDirectoryURL = currentFileURL.deletingLastPathComponent()
-
-    let fileURL =
+extension WrkstrmMain.JSON {
+  public enum Resource {
+    static func load(fileName: String) -> Data? {
+      let currentFileURL = URL(fileURLWithPath: #file)
+      let currentDirectoryURL = currentFileURL.deletingLastPathComponent()
+      
+      let fileURL =
       currentDirectoryURL
-      .appendingPathComponent("Resources", isDirectory: true)
-      .appendingPathComponent(fileName)
-      .appendingPathExtension("json")
-
-    return try? Data(contentsOf: fileURL)
+        .appendingPathComponent("Resources", isDirectory: true)
+        .appendingPathComponent(fileName)
+        .appendingPathExtension("json")
+      
+      return try? Data(contentsOf: fileURL)
+    }
   }
 }
