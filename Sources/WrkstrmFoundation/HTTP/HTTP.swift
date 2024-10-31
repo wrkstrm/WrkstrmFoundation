db5fc0444d1a8c46302720ce5c08fd07ae49884e
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2024-10-31 10:44:41 -0700

    Add HTTP namespace.

---
+++ Sources/WrkstrmFoundation/HTTP/HTTP.swift
@@ -0,0 +1,10 @@
+/// A namespace for HTTP-related functionality.
+enum HTTP {
+  /// A namespace for HTTP status codes.
+  enum StatusCode: Int {
+    /// Represents the HTTP status code for a successful request.
+    ///
+    /// - 200 OK: The request has succeeded.
+    case ok = 200
+  }
+}
