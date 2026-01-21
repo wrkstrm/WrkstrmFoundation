124a234f49fc9da734d7c2b7e5df5d8acef66d87
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2026-01-21 14:25:54 -0800

    Downgrade common-log dependency to version 3.0.0
    Changed the remote dependency for common-log from version 4.0.0 to 3.0.0 in Package.swift. This may be required for compatibility with other packages or to address issues with the newer version.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ Package.Inject.local.dependencies = [
 ]
 
 Package.Inject.remote.dependencies = [
-  .package(url: "https://github.com/wrkstrm/common-log.git", from: "4.0.0"),
+  .package(url: "https://github.com/wrkstrm/common-log.git", from: "3.0.0"),
   .package(url: "https://github.com/wrkstrm/WrkstrmMain.git", from: "2.4.0"),
 ]
 
